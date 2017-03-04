839dbfb21c361a8022a6a6f0ba38966a263ff6e2
Joss Vrooman <Joss Vrooman>
2017-03-04 12:10:51 -0800

    ds

--- app/main.py
+++ app/main.py
@@ -37,6 +37,8 @@ def move():
     # TODO: Do things with data
     directions = ['up', 'down', 'left', 'right']
     final_dir = 2
+    if (len(data.food == 5)
+        final_dir = 2
 
     return {
         #'move': random.choice(directions),
