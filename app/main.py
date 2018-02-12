a268dab84e1bf31634146d83f140173a0146737d
Rowena Zhu <rowzhu@gmail.com>
2018-02-12 12:01:56 -0800

    added snake's name

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ def start():
         'color': '#00FF00',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
-        'name': 'battlesnake-python'
+        'name': 'gucci snake'
     }
 
 
